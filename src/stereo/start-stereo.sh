3fe66b5c7a423fb415b4c9826d9f1d41beea7f5a
Sergei Grichine <msghub@hotmail.com>
2026-03-18 13:23:50 -0500

    Added service files for headless stereo camera process

---
+++ src/stereo/start-stereo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "Waiting for nvargus-daemon and /tmp/argus_socket..."
+
+# must start in '/home/jetson/jetson_nano_b01/src/stereo' directory
+
+for i in {1..60}; do
+    if systemctl is-active --quiet nvargus-daemon && [ -S /tmp/argus_socket ]; then
+        echo "Argus is ready"
+        # Edith the ROS2 node host name (or IP address) and other arguments below:
+        exec ./disparity_server.py --udp-ip <yourhost>.local --udp-port 5005 --no-display --grid-size 10
+    fi
+    sleep 2
+done
+
+echo "Argus did not become ready in time"
+exit 1
+
